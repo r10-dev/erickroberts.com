c265542fcff2712a463b7fd111411734f805cfdd
Erick <erick@pop-os.localdomain>
2019-06-28 22:16:02 -0500

    updated buttercms

--- src/app/services/butterCMS.service.ts
+++ src/app/services/butterCMS.service.ts
@@ -1,4 +1,4 @@
 import * as Butter from 'buttercms';
 
-export const butterService = Butter('your_api_token');
+export const butterService = Butter('0e91733195dbb5b11f41b3732ce0c89623c3f6b6');
 // replace `your_api_token` with the token provided to you by ButterCMS
