5c6f4e051624f74ce6a08ed3d90b7ef8505816e6
Erick <erick@pop-os.localdomain>
2019-06-28 22:04:48 -0500

    modules updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BlogPostDetailsComponent} from './blog-post-details/blog-post-details.component';
+import {BlogPostListingComponent} from './blog-post-listing/blog-post-listing.component';
+import { AppMaterialModule } from './shared';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostDetailsComponent,
+    BlogPostListingComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+        AppRoutingModule,
+        FlexLayoutModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
