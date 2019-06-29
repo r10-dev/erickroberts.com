6a0a807ad5b0a654a8d882960e5d5552b41ddcdb
Erick <erick@pop-os.localdomain>
2019-06-28 22:06:03 -0500

    updated routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlogPostListingComponent } from './blog-post-listing/blog-post-listing.component';
+import { BlogPostDetailsComponent } from './blog-post-details/blog-post-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blog', component: BlogPostListingComponent},
+  {path: 'blog/:slug', component: BlogPostDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
